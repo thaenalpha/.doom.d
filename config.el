33f13117666a3709888927b04f62efe53d60cb0c
Nopanun Laochunhanun <nopanun@live.com>
2021-09-11 22:31:57 +0700

    update mu4e config

--- config.el
+++ config.el
@@ -54,8 +54,10 @@
 
 ; a `fill-column' indicator
 (add-hook 'doom-first-buffer-hook #'global-display-fill-column-indicator-mode)
-(setq sendmail-program "/usr/bin/msmtp"
+
+(after! mu4e
+  (setq sendmail-program (executable-find "msmtp")
         send-mail-function #'smtpmail-send-it
         message-sendmail-f-is-evil t
         message-sendmail-extra-arguments '("--read-envelope-from")
-        message-send-mail-function #'message-send-mail-with-sendmail)
+        message-send-mail-function #'message-send-mail-with-sendmail))
