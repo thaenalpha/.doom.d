c097bbfd0167a24585a202f6cd2d259a85ff48fd
Nopanun Laochunhanun <nopanun@live.com>
2021-12-11 12:31:35 +0700

    packages: format,resolve conflict tangle from conf

--- packages.el
+++ packages.el
@@ -1,2 +1,11 @@
-(package! lsp-tailwindcss
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+(package! arrayify
+  :recipe (:host github :repo "thaenalpha/.doom.d"
+           :files ("lisp/arrayify.el")))
+;; ~/.doom.d/lisp/arrayify.el
+(package! github-search)
+(package! keychain-environment)
+(package! lsp-tailwindcss 
   :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
