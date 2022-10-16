4cde41c03da4926fdcf17851ef67dfff51a7b400
Nopanun Loachunhanun <nopanun@pm.me>
2022-10-16 14:10:03 +0700

    tweak(org): style to match with org-fill-paragraph
    Signed-off-by: Nopanun Loachunhanun <nopanun@pm.me>

--- config.el
+++ config.el
@@ -625,7 +625,8 @@ Also immediately enables `mixed-pitch-modes' if currently in one of the modes."
         ;; use g{h,j,k} to traverse headings and TAB to toggle their
         ;; visibility and leave C-left/C-right to .
         org-tree-slide-skip-outline-level 2
-        org-display-remote-inline-images 'cache)
+        org-display-remote-inline-images 'cache
+        org-startup-indented nil)
   (after! org-capture
     (defun org-capture-select-template-prettier (&optional keys)
       "Select a capture template, in a prettier way than default
